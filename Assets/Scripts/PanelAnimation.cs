d4c3340a509fbb18e01db0dcb523b91abae4e0f0
Marcus Kuffner <marcuskuffner3125@gmail.com>
2023-02-13 18:32:12 -0500

    Added aspect ratio bars animation
    Thanks Sam

--- Assets/Scripts/PanelAnimation.cs
+++ Assets/Scripts/PanelAnimation.cs
@@ -6,18 +6,34 @@ public class PanelAnimation : MonoBehaviour
 {
 
     public float focusHeight = 100;
+    public float lerpIn = 0.05f;
+    public float lerpOut = 0.1f;
     private RectTransform rectTransform;
+    private Vector2 initPosition;
 
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
+        initPosition = rectTransform.anchoredPosition;
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
 
-        //if (Input.GetMouseButton(0)) rectTransform.rect.Set();
+        if (Input.GetMouseButton(0))
+        {
 
+            rectTransform.anchoredPosition = Vector2.Lerp(rectTransform.anchoredPosition, initPosition + new Vector2(0, focusHeight), lerpIn);
+
+        }
+
+        else
+        {
+
+            rectTransform.anchoredPosition = Vector2.Lerp(rectTransform.anchoredPosition, initPosition, lerpOut);
+
+        }
     }
 }
