76d3b45682e696dbf8d1e6b4110f62e954743e25
Marcus Kuffner <marcuskuffner3125@gmail.com>
2023-02-24 17:11:25 -0500

    FinalCommit
    AHHHH

--- Assets/Scripts/DontDestroy.cs
+++ Assets/Scripts/DontDestroy.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DontDestroy : MonoBehaviour
 {
 
-    private void Awake()
+    private void Start()
     {
         DontDestroyOnLoad(transform.gameObject);
+        SceneManager.LoadScene(sceneName: "MainMenu");
     }
 
 }
